76089590e1f52b0ce982484e16f870cabc43f39f
Royson Immanuel <royson.immanuel@autolabs.ai>
2018-12-18 12:09:55 +0530

    sds

--- app.js
+++ app.js
@@ -2,13 +2,23 @@
 
 var express = require('express');
 var app = express();
-var port = process.env.port || 3004
+var port = process.env.port || 8090
+
+let jsonData = require('./data');
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.get('/',function(req, res, next){
-	res.send('mlm-distributors');
+	res.send('prod-service');
 });
-app.get('/getDistributors',function(req, res, next){
-	res.sendFile(__dirname + '/data.json');
+app.get('/products',function(req, res, next){
+	res.send(jsonData);
 });
 
 app.listen(port);
+
+console.log('server running at '+port)
